db6eba45287ba0f66adf4ab21b73f894a35a504e
User <kirill.kalakin@gmail.com>
2022-01-25 23:14:34 -1200

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,26 @@
+package ru.job4j.math;
+
+public class MathFunction {
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+}
+
+package ru.job4j.calculator;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return ru.job4j.math.MathFunction.sum(first, second)
+                + ru.job4j.math.MathFunction.multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+}
+
