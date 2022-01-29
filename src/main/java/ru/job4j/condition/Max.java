bbf24b2208059e74a29d6e628c037cbd459a017a
User <kirill.kalakin@gmail.com>
2022-01-28 21:46:14 -1200

    4.2. Тернарное сравнение [#189]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,12 +2,11 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int fragment =  left > right ? left : right;
-        return fragment;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
-        int msg = Max.max(200, 50);
+        int msg = Max.max(200, 500);
         System.out.println(msg);
     }
 }
