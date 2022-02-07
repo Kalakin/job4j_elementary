2f7f8b030c173470f675e9a5a288f70722d64498
User <kirill.kalakin@gmail.com>
2022-02-06 20:19:14 -1200

    5.4. Протеиновая диета [#156313]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -2,10 +2,11 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        int month = 2;
-        while (ivan * month > nik * month) {
-            month--;
+        int month = 0;
+        while (ivan <= nik) {
             ivan *= 3;
+            nik *= 2;
+            month++;
         }
         return month;
     }
