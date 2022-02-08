4c6a67524b8d2565aa11bff5169f6bc88a12afe6
User <kirill.kalakin@gmail.com>
2022-02-07 13:26:40 -1200

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен " + ages.length);
         System.out.println("Размер массива равен " + surnames.length);
         System.out.println("Размер массива равен " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Kirill Kalakin";
+        names[1] = "Andrey Kalakin";
+        names[2] = "Larisa Kalakina";
+        names[3] = "Georgy Kalakin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
