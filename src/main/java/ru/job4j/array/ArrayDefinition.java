5985769a10296dff3779e4ca7ef4d9cf496688c7
User <kirill.kalakin@gmail.com>
2022-02-08 23:07:32 -1200

    6.0.3. Массивы и цикл for. [#226665]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,20 +2,13 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива равен " + ages.length);
-        System.out.println("Размер массива равен " + surnames.length);
-        System.out.println("Размер массива равен " + prices.length);
         String[] names = new String[4];
         names[0] = "Kirill Kalakin";
         names[1] = "Andrey Kalakin";
         names[2] = "Larisa Kalakina";
         names[3] = "Georgy Kalakin";
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
     }
 }
