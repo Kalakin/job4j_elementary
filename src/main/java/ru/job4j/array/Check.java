0bab5031aae4b347377673a6c9ba80f8f2fae646
User <kirill.kalakin@gmail.com>
2022-02-13 22:28:55 -1200

    6.4. Слово начинается с ... [#41585]

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,9 +3,9 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        for (int i = 1; i < data.length; i++) {
-            if (data[i] == false) {
-                boolean result = false;
+        for (boolean i : data) {
+            if (!i) {
+                return false;
             }
         }
         return result;
