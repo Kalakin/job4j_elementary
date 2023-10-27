807de462a97bf8a5fbcef71aa7724b20b595e781
User <kirill.kalakin@gmail.com>
2023-10-27 21:06:50 +0300

    6. Правила Checkstyle [#504981]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -3,22 +3,22 @@ package ru.job4j.checkstyle;
 public class Broken {
     private int size = 10;
 
+    String name;
+
     public String surname;
 
-    String name;
+    public static final String NEWVALUE = "";
 
     Broken() { }
 
     void echo() { }
+
+    void method(int a, int b, int c) {
+    }
 
     void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
-
-    void method(int a, int b, int c) {
-    }
-
-    public static final String NEWVALUE = "";
 }
